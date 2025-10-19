6279164b378fcee80d65d24e6f2d63347224bc5c
Jawad Saeed <114721996+Jawamegamind@users.noreply.github.com>
2025-10-18 20:13:42 -0400

    Updated db to not load env variables at module level and added test cases for testing supabase db connection

--- backend/database/supabase_db.py
+++ backend/database/supabase_db.py
@@ -2,11 +2,14 @@ from supabase import Client, create_client
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-project_url = os.getenv("PROJECT_URL")
-project_key = os.getenv("API_KEY")
-
 def create_supabase_client():
+    """Create and return a Supabase client"""
+    # Load environment variables
+    load_dotenv()
+    
+    # Get environment variables at runtime
+    project_url = os.getenv("PROJECT_URL")
+    project_key = os.getenv("API_KEY")
+    
     supabase: Client = create_client(project_url, project_key)
     return supabase
